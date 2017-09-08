bf39c2883b3a835cfef78ebd8a992178c333599e
Joelforr_MSI-Stealth <joelforr@gmail.com>
2017-09-08 15:16:31 -0400

    Inheritance Lesson

---
+++ WarmUp/Assets/Scripts/NumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGenerator : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual int Next()
+    {
+        return (int)Random.Range(0, 4);
+    }
+}
